b146a80b897e7f57815eccfe292c008165d23544
Mark Fannon <markjfannon@Markbook-Pro.local>
2023-01-28 21:41:18 +0000

    Added blog excerpts/info to navigation page

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { graphq, graphql, useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const Seo = ({title}) => {
     const data = useStaticQuery(graphql`
