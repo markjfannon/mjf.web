a55f6485763e13cdcd45b8aad313ca6cd529144d
Mark Fannon <markjfannon@Markbook-Pro.local>
2023-01-28 22:33:15 +0000

    The blog mainpage now has links directing to individual articles

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,11 @@ const AboutPage = () => {
         <main>
             <Layout pageTitle="About Me">
             <p>Hi there! I'm Mark, a computer science student at the <a href="https://cs.nott.ac.uk">University of Nottingham</a>.</p>
+
+            <p>On the side, I present on <a href="https://urn1350.net">University Radio Nottingham</a>, alongside having technical and audio production duties.
+            My presenting ranges from hosting "The Pulse" (a daily news show) to specialist music programming.</p>
+
+            <p>In addition, I am a self taught musician and play the bass guitar, electric guitar, piano, keyboard and drums.</p>
             </Layout>
         </main>
     )
