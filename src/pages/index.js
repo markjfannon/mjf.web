272d9ddf5fb66b8bc39408e3a3cf6ba9417d72a1
Mark Fannon <markjfannon@Markbook-Pro.local>
2023-01-28 17:20:23 +0000

    added a cool image of me to the homepage. woah

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby tutorial!</p>
+      <p>Welcome to the page of Mark!</p>
+      <StaticImage
+      alt="Mark"
+      src="../images/stare.jpg"
+      />
       </Layout>
     </main>
   )
